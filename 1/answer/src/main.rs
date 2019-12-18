7cacc99b4b573f85f2880071bed09602fb7a1f9f
multi prise <korokoko.toi@gmail.com>
2019-12-18 08:55:50 +0100

    added the new thingy

---
+++ 1/answer/src/main.rs
@@ -0,0 +1,39 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_input() -> std::string::String {
+    let path = Path::new("../1.txt");
+    let display = path.display();
+
+    let mut file = match File::open(&path){
+        Err(why) => panic!("couldn't open the file {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}:  {}", display, why.description()),
+        Ok(_) => ()
+    };
+    s
+}
+
+
+fn main() {
+    let s = read_input();
+    let split: Vec<i32> = s.split_whitespace().
+        map(|x| x.parse::<i32>().unwrap())
+        .collect();
+    let res = split.iter().fold(0, |a, b| a + b / 3  - 2);
+
+    // Second answer
+
+    let mut res2 = 0;
+    let mut additional_ful = res / 3 - 2;
+    while additional_ful > 0 {
+        res2 += additional_ful;
+        additional_ful = additional_ful / 3 - 2;
+    }
+    println!("result: 1 {} 2 {}", res, res2);
+}
