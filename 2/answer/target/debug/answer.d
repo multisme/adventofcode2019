0df27c16da1ef216d9b93109835a433b7bea02ce
multi prise <korokoko.toi@gmail.com>
2019-12-18 17:49:57 +0100

    .gitignore fix

--- 2/answer/target/debug/answer.d
+++
@@ -1,1 +0,0 @@
-/home/killua/adventofcode/2/answer/target/debug/answer: /home/killua/adventofcode/2/answer/src/main.rs
--- 2/answer/target/debug/deps/answer-c6313389d86a4435.d
+++
@@ -1,5 +0,0 @@
-/home/killua/adventofcode/2/answer/target/debug/deps/answer-c6313389d86a4435: src/main.rs
-
-/home/killua/adventofcode/2/answer/target/debug/deps/answer-c6313389d86a4435.d: src/main.rs
-
-src/main.rs:
