2931902756f435751cbaae772624994231798e83
multi prise <korokoko.toi@gmail.com>
2020-09-03 10:47:53 +0200

    started then 10

---
+++ 10/answer/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    let file_input = read_input("../10.1.txt");
+    //Get the right data from the input
+    let input: &str = match file_input.split_whitespace().next() {
+        Some(s) => s,
+        None => ""
+    };
+}
+
+fn read_input(file_location: &str) -> std::string::String {
+    let path = Path::new(file_location);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open the file {}: {}", display, why.to_string()),
+        Ok(file) => file,
+    };  
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}:  {}", display, why.to_string()),
+        Ok(_) => (), 
+    };
+    s   
+}
+
+
